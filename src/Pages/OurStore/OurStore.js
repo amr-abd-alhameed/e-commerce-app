6712901ab6d2d7392ac4bced11771135c698cb9a
amr <aa5473@fayoum.edu.eg>
2023-03-14 21:48:23 +0200

    some sections

--- src/Pages/OurStore/OurStore.js
+++ src/Pages/OurStore/OurStore.js
@@ -2,8 +2,11 @@ import React from "react";
 import Meta from "../../Components/commonComponents/Meta";
 import Title from "../../Components/commonComponents/Title";
 import Wrapper from "../../Components/commonComponents/Wrapper";
+import CardLeftFour from "./Components/CardLeftFour";
 import CardLeftOne from "./Components/CardLeftOne";
+import CardLeftThree from "./Components/CardLeftThree";
 import CardLeftTwo from "./Components/CardLeftTwo";
+import logo1 from "../../assets/images/watch.jpg";
 
 const OurStore = () => {
     return (
@@ -25,9 +28,23 @@ const OurStore = () => {
                         />
                         <CardLeftTwo
                             title={"filter by"}
-                            subTitle={"availability"}
+                            subTitle1={"availability"}
+                            subTitle2={"price"}
                             text1={"in stock"}
                             text2={"out of stock"}
+                        />
+                        <CardLeftThree
+                            title={"product tags"}
+                            text1={"samsung"}
+                            text2={"lg"}
+                            text3={"apple"}
+                            text4={"vivo"}
+                            text5={"amr"}
+                        />
+                        <CardLeftFour
+                            title={"random products"}
+                            value={4}
+                            logo1={logo1}
                         />
                     </div>
                 </Wrapper>
