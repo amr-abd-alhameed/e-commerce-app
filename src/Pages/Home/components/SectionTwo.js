212cebfe025fec6b9252c76fd948214b8777ce0f
amr <aa5473@fayoum.edu.eg>
2023-05-09 14:24:56 +0300

    finishing section two in home page

--- src/Pages/Home/components/SectionTwo.js
+++ src/Pages/Home/components/SectionTwo.js
@@ -7,15 +7,68 @@ import logo3 from "../../../assets/images/service-02.png";
 import logo4 from "../../../assets/images/service-03.png";
 import logo5 from "../../../assets/images/service-04.png";
 import logo2 from "../../../assets/images/service-05.png";
+import shortid from "shortid";
 
 const SectionTwo = () => {
+    const smallBlocks = [
+        {
+            logo: logo1,
+            alt: "service",
+            text: "from all order over $100",
+            title: "free shipping",
+            className: "",
+            id: shortid.generate(),
+        },
+        {
+            logo: logo2,
+            alt: "service",
+            text: "save up to 75% off",
+            title: "daily surprise offers",
+            className: "",
+            id: shortid.generate(),
+        },
+        {
+            logo: logo3,
+            alt: "service",
+            text: "shop with an expert",
+            title: " support 24/7",
+            className: "",
+            id: shortid.generate(),
+        },
+        {
+            logo: logo4,
+            alt: "service",
+            text: "get factor direct price",
+            title: "affordable prices",
+            className: "",
+            id: shortid.generate(),
+        },
+        {
+            logo: logo5,
+            alt: "service",
+            text: "secure payments",
+            title: "100% protected payments",
+            className: "last",
+            id: shortid.generate(),
+        },
+    ];
     return (
         <>
             <section className="home-wrapper-2 py-5">
                 <Wrapper>
                     <div className="col-12">
                         <div className="services d-flex align-items-center justify-content-between">
-                            <SmallBlock
+                            {smallBlocks.map((smallBlock) => (
+                                <SmallBlock
+                                    key={smallBlock.id}
+                                    logo={smallBlock.logo}
+                                    alt={smallBlock.alt}
+                                    text={smallBlock.text}
+                                    title={smallBlock.title}
+                                    className={smallBlock.className}
+                                />
+                            ))}
+                            {/* <SmallBlock
                                 logo={logo1}
                                 alt={"service"}
                                 text={"from all order over $100"}
@@ -44,7 +97,7 @@ const SectionTwo = () => {
                                 alt={"service"}
                                 text={"secure payments"}
                                 title={"100% protected payments"}
-                            />
+                            /> */}
                         </div>
                     </div>
                 </Wrapper>
--- src/Pages/Home/components/SmallBlock.js
+++ src/Pages/Home/components/SmallBlock.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SmallBlock = ({ logo, alt, title, text }) => {
+const SmallBlock = ({ logo, alt, title, text, className }) => {
     return (
         <>
-            <div className="d-flex align-items-center gap-10">
+            <div className={`d-flex align-items-center gap-10 ${className}`}>
                 <img src={logo} alt={alt} />
                 <div>
                     <h6 className="text-capitalize">{title}</h6>
