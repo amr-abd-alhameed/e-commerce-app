9f2217f3143e39a80aec44ecc94e8d9035d24713
amr <aa5473@fayoum.edu.eg>
2023-07-28 16:02:24 +0300

    fixing some problems in bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
